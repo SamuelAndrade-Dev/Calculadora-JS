ebe417232917bed788bb0cbaa8fd5a6bfabef968
Samuel Andrade <samuelandrade.spa@gmail.com>
2024-11-18 22:43:47 -0300

    reparação de erros em JS

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
 const result = document.querySelector(".result"); // pegar o resultado
 const buttons  = document.querySelectorAll(".buttons button"); // pegar todos os buttons (todos os td)
-
 // criar variaveis de calculos
-
 let currentNumber = "";       //numero atual
 let firstOpe = null;          //primeiro numero
 let operador = null;          //operador
 let restart = false;          // Restart 
-
 //funcoes: 
-
 fuction addDigit(digit){    //funcao para add o digito no visor
     if (addDigit === "," && (currentNumber.includes(",") ))
 }
-
 //para cada botão (TD)
-
 buttons.forEach(button) => {           //estudar forEach
     button.addEventListener("click", () => {
         const textoBotao = button.innertext;
