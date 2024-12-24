68276dc22b1397e9acc9549b6a964052f8aac54a
Samuel Andrade <samuelandrade.spa@gmail.com>
2024-12-24 18:54:30 -0300

    Funções adicionadas

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
-const numberTd = document.querySelectorAll('[appen-number]')
-const operatorTd = document.querySelectorAll('[append-operator]')
-const equalsTd = document.querySelectorAll('[append-equals]')
-const deleteTd = document.querySelectorAll('[append-delete]')
-const allClearTd = document.querySelectorAll('[append-all-clear]')
+function appendNumber(number) {
+    const display = document.getElementById('display');
+    display.value += number;
+}
+
+function appendOperator(operator) {
+    const display = document.getElementById('display');
+    const lastChar = display.value.slice(-1)
+
+    if (display.value !== '' && !['+', '-', '*', '/'].includes(lastChar)) {
+        display.value += operator;
+     }
+}
+
+function clean()
+{
+    document.getElementById('display').value = "";
+}
+
+function back() {
+    const display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        display.value = 'Erro';
+    }
+}
